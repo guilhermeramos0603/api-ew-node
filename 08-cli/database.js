71f6931f8f2a42d6cd62c3a3c82de5f6d3f2ed96
Guilherme Ramos <guilherme.silvano@hotmail.com>
2022-10-15 19:40:59 -0300

    CRUD-COMPLETE-NOBANK

--- 08-cli/database.js
+++ 08-cli/database.js
@@ -1,22 +1,24 @@
-const {readFile, writeFile} = require('fs')
-const {promisify} = require('util')
+const { readFile, writeFile } = require('fs')
+const { promisify } = require('util')
 
 const readFileAsync = promisify(readFile)
 const writeFileAsync = promisify(writeFile)
 
 class Database {
-    constructor(){
+    constructor() {
         this.NAME_FILE = 'heros.json'
     }
     async getDataFile() {
         const file = await readFileAsync(this.NAME_FILE, 'utf8')
         return JSON.parse(file.toString())
     }
-    async writeHere(data){
+    async writeHere(data) {
         await writeFileAsync(this.NAME_FILE, JSON.stringify(data))
         return true
     }
-    async store(hero){
+
+
+    async create(hero) {
         const data = await this.getDataFile()
         const id = hero.id <= 2 ? hero.id : Date.now()
         const heroWithId = {
@@ -31,10 +33,38 @@ class Database {
         const result = await this.writeHere(finishedData)
         return result
     }
-    async list(id) {
+
+    async read(id) {
         const data = await this.getDataFile()
         const filterData = data.filter(item => (id ? (item.id === id) : true))
         return filterData
+    }
+
+    async update(id, changes) {
+        const data = await this.getDataFile()
+        const index = data.findIndex(item => item.id === parseInt(id))
+        if (index === -1) {
+            throw Error('Hero not found with id informed')
+        }
+        const actual = data[index]
+        data.splice(index, 1)
+
+        const changesObj = JSON.parse(JSON.stringify(changes))
+        const attData = Object.assign({}, actual, changesObj)
+        return await this.writeHere([...data, attData])
+    }
+
+    async delete(id) {
+        if (!id) {
+            return await this.writeHere([])
+        }
+        const data = await this.getDataFile()
+        const index = data.findIndex(item => item.id === parseInt(id))
+        if (index === -1) {
+            throw Error('Hero not found with id informed')
+        }
+        data.splice(index, 1)
+        return await this.writeHere(data)
     }
 }
 
--- 08-cli/test.js
+++ 08-cli/test.js
@@ -1,20 +1,51 @@
 const { deepEqual, ok } = require('assert')
 const Database = require('./database')
 
-const DEFAULT_ITEM_REGISTER = {name:'Flash', power: 'Speed', id: 1} 
+const DEFAULT_ITEM_REGISTER = { name: 'Flash', power: 'Speed', id: 1 }
+const DEFAULT_ITEM_UPDATE = { name: 'Green Lantern', power: 'Ring energy', id: 2 }
 
 describe('Hero manipulation suite', () => {
-    it('Search a hero using files', async () => {
-        const expected = DEFAULT_ITEM_REGISTER 
-        const [result] = await Database.list(expected.id)
+    before(async () => {
+        await Database.delete();
+        await Database.create(DEFAULT_ITEM_REGISTER);
+        await Database.create(DEFAULT_ITEM_UPDATE);
+      });
+    it('Create a hero using files', async () => {
+        const expected = DEFAULT_ITEM_REGISTER
+        const result = await Database.create(DEFAULT_ITEM_REGISTER)
+        const [actual] = await Database.read(DEFAULT_ITEM_REGISTER.id)
+        deepEqual(actual, expected)
+    })
+
+    it('Read a hero using files', async () => {
+        const expected = DEFAULT_ITEM_REGISTER
+        const [result] = await Database.read(expected.id)
         deepEqual(result, expected)
     })
 
-    it('Search a hero using files', async () => {
-        const expected = DEFAULT_ITEM_REGISTER 
-        const result = await Database.store(DEFAULT_ITEM_REGISTER)
-        const [actual] = await Database.list(DEFAULT_ITEM_REGISTER.id)
-        deepEqual(actual, expected)
+    it('Update a hero by id', async () => {
+        const expected = {
+            ...DEFAULT_ITEM_UPDATE,
+            name: 'Batman',
+            power: 'Money'
+        }
+
+        await Database.update(expected.id, {
+            name: expected.name,
+            power: expected.power
+        })
+
+        const [realResult] = await Database.read(expected.id)
+        deepEqual(realResult, expected)
+
     })
+
+    it('Delete a hero by id', async () => {
+        const expected = true
+        const result = await Database.delete(DEFAULT_ITEM_REGISTER.id)
+        deepEqual(result, expected)
+
+    })
+
 
 })
